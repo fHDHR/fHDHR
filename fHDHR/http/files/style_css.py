d2fbd11f31f5cd429e1ca0ec34f88992f53ce783
deathbybandaid <github@deathbybandaid.net>
2020-12-02 09:40:48 -0500

    Allow Cross Origin CSS Theming via cache

--- fHDHR/http/files/style_css.py
+++ fHDHR/http/files/style_css.py
@@ -1,4 +1,6 @@
-from flask import send_from_directory
+from flask import Response
+import pathlib
+from io import StringIO
 
 
 class Style_CSS():
@@ -8,10 +10,35 @@ class Style_CSS():
     def __init__(self, fhdhr):
         self.fhdhr = fhdhr
 
+        self.internal_style_file = pathlib.Path(
+            self.fhdhr.config.internal["paths"]["www_dir"]).joinpath('style.css')
+
+        self.internal_style = StringIO()
+        self.internal_style.write(open(self.internal_style_file).read())
+
+        self.pull_external_theme()
+
+    def pull_external_theme(self):
+        self.external_style = None
+        self.external_style_address = None
+        if self.fhdhr.config.dict["web_ui"]["theme"]:
+            if self.fhdhr.config.dict["web_ui"]["theme"].startswith(tuple(["http://", "https://"])):
+                css_req = self.fhdhr.web.session.get(self.fhdhr.config.dict["web_ui"]["theme"])
+                self.external_style = StringIO(css_req.text)
+                self.external_style_address = self.fhdhr.config.dict["web_ui"]["theme"]
+
     def __call__(self, *args):
         return self.get(*args)
 
     def get(self, *args):
 
-        return send_from_directory(self.fhdhr.config.internal["paths"]["www_dir"],
-                                   'style.css')
+        main_output = StringIO()
+
+        main_output.write(self.internal_style.getvalue())
+        if self.fhdhr.config.dict["web_ui"]["theme"]:
+            if self.fhdhr.config.dict["web_ui"]["theme"] != self.external_style_address:
+                self.pull_external_theme()
+            if self.external_style:
+                main_output.write(self.external_style.getvalue())
+
+        return Response(status=200, response=main_output.getvalue(), mimetype="text/css")
