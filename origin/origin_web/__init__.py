e41ffe81f63d49f5add3e6d011d0b571a9eb6178
deathbybandaid <github@deathbybandaid.net>
2020-12-10 10:27:58 -0500

    Add API Tool to Look at other DMA Channels without listings

--- origin/origin_web/__init__.py
+++ origin/origin_web/__init__.py
@@ -1,5 +1,6 @@
 
 from .origin_api import Origin_API
+from .origin_api_tools import Origin_API_Tools
 from .origin_html import Origin_HTML
 
 
@@ -9,4 +10,5 @@ class fHDHR_Origin_Web():
         self.fhdhr = fhdhr
 
         self.origin_api = Origin_API(fhdhr)
+        self.origin_api_tools = Origin_API_Tools(fhdhr)
         self.origin_html = Origin_HTML(fhdhr)
