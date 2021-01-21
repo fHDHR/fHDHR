ccce786a52144cb574a19bc0ee394ad5105dd85b
deathbybandaid <github@deathbybandaid.net>
2021-01-21 15:20:12 -0500

    Make alt_epg loading more abstract

--- alternative_epg/__init__.py
+++ alternative_epg/__init__.py
@@ -1,3 +1,7 @@
-# pylama:ignore=W0401,W0611
-from .zap2it import *
-from .tvtv import *
+import os
+
+alt_epg_top_dir = os.path.dirname(__file__)
+for entry in os.scandir(alt_epg_top_dir):
+    if entry.is_dir() and not entry.is_file() and entry.name[0] != '_':
+        imp_string = "from .%s import *" % entry.name
+        exec(imp_string)
