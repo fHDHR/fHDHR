095b92b07bfac33a3ee70323e36855bd6c852936
deathbybandaid <github@deathbybandaid.net>
2021-02-03 14:27:45 -0500

    Add Config Help Button to WebUI

--- fHDHR_web/pages/settings_html.py
+++ fHDHR_web/pages/settings_html.py
@@ -29,6 +29,8 @@ class Settings_HTML():
                         "value": self.fhdhr.config.dict[config_section][config_item],
                         "value_default": self.fhdhr.config.conf_default[config_section][config_item]["value"],
                         "hide": self.fhdhr.config.conf_default[config_section][config_item]["config_web_hidden"],
+                        "valid_options": self.fhdhr.config.conf_default[config_section][config_item]["valid_options"],
+                        "description": self.fhdhr.config.conf_default[config_section][config_item]["description"],
                         "configurable": configurable
                         }
             if not len(web_settings_dict[config_section].keys()):
