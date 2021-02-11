d442363e43df685838a3b01e2deba2627dd3a978
deathbybandaid <github@deathbybandaid.net>
2021-02-11 09:53:52 -0500

    Enhance Logger with dictconfig

--- fHDHR/logger/__init__.py
+++ fHDHR/logger/__init__.py
@@ -1,36 +1,49 @@
 import os
 import logging
+from logging.config import dictConfig
 
 
 class Logger():
 
     def __init__(self, settings):
-        self.config = settings
-
-        log_level = self.config.dict["logging"]["level"].upper()
-
-        # Create a custom logger
-        logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s', level=log_level)
+        logging_config = {
+            'version': 1,
+            'formatters': {
+                'fHDHR': {
+                    'format': '[%(asctime)s] %(levelname)s - %(message)s',
+                    },
+            },
+            'loggers': {
+                # all purpose, fHDHR root logger
+                'fHDHR': {
+                    'level': settings.dict["logging"]["level"].upper(),
+                    'handlers': ['console', 'logfile'],
+                },
+            },
+            'handlers': {
+                # output on stderr
+                'console': {
+                    'level': 'DEBUG',
+                    'class': 'logging.StreamHandler',
+                    'formatter': 'fHDHR',
+                },
+                # generic purpose log file
+                'logfile': {
+                    'level': 'DEBUG',
+                    'class': 'logging.handlers.TimedRotatingFileHandler',
+                    'filename': os.path.join(
+                        settings.internal["paths"]["logs_dir"], '.fHDHR.log'),
+                    'when': 'midnight',
+                    'formatter': 'fHDHR',
+                },
+            },
+        }
+        dictConfig(logging_config)
         self.logger = logging.getLogger('fHDHR')
-        log_file = os.path.join(self.config.internal["paths"]["logs_dir"], 'fHDHR.log')
-
-        # Create handlers
-        # c_handler = logging.StreamHandler()
-        f_handler = logging.FileHandler(log_file)
-        # c_handler.setLevel(log_level)
-        f_handler.setLevel(log_level)
-
-        # Create formatters and add it to handlers
-        # c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-        f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        # c_handler.setFormatter(c_format)
-        f_handler.setFormatter(f_format)
-
-        # Add handlers to the logger
-        # logger.addHandler(c_handler)
-        self.logger.addHandler(f_handler)
 
     def __getattr__(self, name):
         ''' will only get called for undefined attributes '''
         if hasattr(self.logger, name):
             return eval("self.logger.%s" % name)
+        elif hasattr(self.logger, name.lower()):
+            return eval("self.logger.%s" % name.lower())
