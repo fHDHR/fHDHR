08a6b8fc29a17e62f3c780b3bff9f0e599959d55
Sam Zick <github@deathbybandaid.net>
2021-03-01 09:02:46 -0500

    Revert MonkeyPatching

--- main.py
+++ main.py
@@ -9,9 +9,6 @@ SCRIPT_DIR = pathlib.Path(os.path.dirname(os.path.abspath(__file__)))
 from fHDHR.deps import Dependencies
 deps = Dependencies(SCRIPT_DIR)
 
-from gevent import monkey
-monkey.patch_all(ssl=False)
-
 from fHDHR.cli import run
 import fHDHR_web
 
