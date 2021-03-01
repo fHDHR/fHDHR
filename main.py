6c7172e8c70b67bf99b2f0437ae19abbf7c6046c
Sam Zick <github@deathbybandaid.net>
2021-03-01 09:23:12 -0500

    Safe Install gevent and restart

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 # coding=utf-8
 # pylama:ignore=E402
+try:
+    from gevent import monkey
+    monkey.patch_all()
+    gevent_check = True
+except ModuleNotFoundError:
+    gevent_check = False
+
 import os
 import sys
 import pathlib
 SCRIPT_DIR = pathlib.Path(os.path.dirname(os.path.abspath(__file__)))
 
-from fHDHR.deps import Dependencies
+from deps import Dependencies
 deps = Dependencies(SCRIPT_DIR)
+if not gevent_check:
+    print("gevent was missing, restarting...")
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    sys.exit()
 
 from fHDHR.cli import run
 import fHDHR_web
