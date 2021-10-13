2f05ae0be9782221656410859c9025532a11e453
deathbybandaid <github@deathbybandaid.net>
2021-10-13 13:52:58 -0400

    Improve Startup Scheduling

--- fHDHR_web/api/startup_tasks.py
+++ fHDHR_web/api/startup_tasks.py
@@ -18,7 +18,43 @@ class Startup_Tasks():
 
         self.fhdhr.logger.noob("Running Startup Tasks.")
 
-        # Hit Channel Update API
+        self.startup_versions_update()
+
+        self.startup_channel_scan()
+
+        self.startup_epg_update()
+
+        self.startup_ssdp_alive()
+
+        self.fhdhr.logger.noob("Startup Tasks Complete.")
+
+        return "Success"
+
+    def startup_epg_update(self):
+
+        for epg_method in self.fhdhr.device.epg.epg_methods:
+            haseverpulled = self.fhdhr.db.get_fhdhr_value("epg", "update_time", epg_method)
+            updateepg = False
+
+            if not haseverpulled:
+                updateepg = True
+
+            elif hasattr(self.fhdhr.device.epg.epg_methods[epg_method]["class"], "epg_update_on_start"):
+                updateepg = self.fhdhr.device.epg.epg_methods[epg_method]["class"].epg_update_on_start
+
+            elif epg_method in list(self.fhdhr.config.dict.keys()):
+                if "epg_update_on_start" in list(self.fhdhr.config.dict[epg_method].keys()):
+                    updateepg = self.fhdhr.config.dict[epg_method]["epg_update_on_start"]
+                else:
+                    updateepg = self.fhdhr.config.dict["fhdhr"]["epg_update_on_start"]
+
+            elif self.fhdhr.config.dict["epg"]["epg_update_on_start"]:
+                updateepg = True
+
+            if updateepg:
+                self.fhdhr.scheduler.run_from_tag("%s EPG Update" % epg_method)
+
+    def startup_channel_scan(self):
         for origin in list(self.fhdhr.origins.origins_dict.keys()):
 
             haseverscanned = self.fhdhr.db.get_fhdhr_value("channels", "scanned_time", origin)
@@ -40,31 +76,10 @@ class Startup_Tasks():
                 updatechannels = self.fhdhr.config.dict["fhdhr"]["chanscan_on_start"]
 
             if updatechannels:
-                self.fhdhr.api.get("%s&origin=%s" % (self.channel_update_url, origin))
+                self.fhdhr.scheduler.run_from_tag("%s Channel Scan" % origin)
 
-        # Hit EPG Update API
-        for epg_method in self.fhdhr.device.epg.epg_methods:
-            haseverpulled = self.fhdhr.db.get_fhdhr_value("epg", "update_time", epg_method)
-            updateepg = False
+    def startup_versions_update(self):
+        self.fhdhr.scheduler.run_from_tag("Versions Update")
 
-            if not haseverpulled:
-                updateepg = True
-
-            elif hasattr(self.fhdhr.device.epg.epg_methods[epg_method]["class"], "epg_update_on_start"):
-                updateepg = self.fhdhr.device.epg.epg_methods[epg_method]["class"].epg_update_on_start
-
-            elif epg_method in list(self.fhdhr.config.dict.keys()):
-                if "epg_update_on_start" in list(self.fhdhr.config.dict[epg_method].keys()):
-                    updateepg = self.fhdhr.config.dict[epg_method]["epg_update_on_start"]
-                else:
-                    updateepg = self.fhdhr.config.dict["fhdhr"]["epg_update_on_start"]
-
-            elif self.fhdhr.config.dict["epg"]["epg_update_on_start"]:
-                updateepg = True
-
-            if updateepg:
-                self.fhdhr.api.get("%s&source=%s" % (self.epg_update_url, epg_method))
-
-        self.fhdhr.logger.noob("Startup Tasks Complete.")
-
-        return "Success"
+    def startup_ssdp_alive(self):
+        self.fhdhr.scheduler.run_from_tag("SSDP Alive")
